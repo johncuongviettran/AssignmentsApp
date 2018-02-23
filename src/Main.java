05c1b9e0e1014ba9836bf336296aedd96358a08e
Danton Vu <vudanton@gmail.com>
2018-02-22 21:34:26 -0600

    Story ID#153893472

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("\n\nHello, AssignmentsApp!\n");
+
+        //Output the current date-time.
+        LocalDateTime today = LocalDateTime.now();
+        System.out.println("\nThe current date-time is: " + today);
+        
+
     }
 }
