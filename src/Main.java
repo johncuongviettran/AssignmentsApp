3f71dfec6604bc5b17394b78630367d6670d61d9
Danton Vu <vudanton@gmail.com>
2018-02-27 21:32:31 -0600

    Simplify FindEarlierDate function.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Random;
 
 public class Main {
 
@@ -30,27 +31,27 @@ public class Main {
         System.out.println("The day of the week of your birthdate was " + birthDate.getDayOfWeek());
 
         //Output the number of days you've been alive.
-        System.out.println("The number of days you have been alive is " + ChronoUnit.DAYS.between(birthDate,today) + " days.");
+        System.out.println("The number of days you have been alive is " + ChronoUnit.DAYS.between(birthDate, today) + " days.");
 
         //Output the number of days between two dates.
-        LocalDateTime obamaInauguration = LocalDateTime.of(2009,2,20, 12,0);
-        System.out.println("The number of days between your birthdate and Obama's inauguration is " + ChronoUnit.DAYS.between(birthDate,obamaInauguration) + " days.");
+        LocalDateTime obamaInauguration = LocalDateTime.of(2009, 2, 20, 12, 0);
+        System.out.println("The number of days between your birthdate and Obama's inauguration is " + ChronoUnit.DAYS.between(birthDate, obamaInauguration) + " days.");
 
-        //Given two dates, output the earlier. Honestly, I don't understand the need to use all three methods (isBefore(), isAfter(), compareTo()) is necessary.
-        LocalDateTime earlierDate = FindEarlierDate(birthDate, today);
-        System.out.println("The earlier date is " + earlierDate);
+        //Given two dates, output the earlier.
+        FindEarlierDate(today, obamaInauguration);
 
-        }
+        
 
-    private static LocalDateTime FindEarlierDate(LocalDateTime date1, LocalDateTime date2) {
-        LocalDateTime earlyDate = date1;
-        int comparedValue = date1.compareTo(date2);
-        if (date1.isBefore(date2) && comparedValue < 0){
-            earlyDate = date1;
-        }else if (date1.isAfter(date2) && comparedValue > 0) {
-            earlyDate = date2;
-        }
-        return earlyDate;
     }
 
+
+    private static void FindEarlierDate(LocalDateTime date1, LocalDateTime date2) {
+        if (date1 == date2) {
+            System.out.println("\nThe dates are the same.");
+        } else if (date1.isBefore(date2)) {
+            System.out.println("The earlier date is " + date1);
+        } else {
+            System.out.println("The earlier date is " + date2);
+        }
+    }
 }
